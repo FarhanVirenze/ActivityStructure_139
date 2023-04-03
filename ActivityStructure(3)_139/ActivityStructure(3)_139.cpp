7f5183e79b5abaecf8074545b76ff290817f73d3
FarhanVirenze <farhanvirenze18@gmail.com>
2023-04-03 11:21:09 +0700

    tambahan fungsi array looping

--- ActivityStructure(3)_139/ActivityStructure(3)_139.cpp
+++ ActivityStructure(3)_139/ActivityStructure(3)_139.cpp
@@ -23,7 +23,22 @@ int main()
     Mahasiswa mhs[3];
 
     for (int i = 0; i < 3; i++) {
-        
+        cout << "Masukan nim :";
+        cin.getline(mhs[i].nim,12);
+        cout << "Masukan Nama :";
+        cin.getline(mhs[i].nama,25);
+        cout << "Alamat :";
+        cout << "\n\tMasukan Desa :";
+        cin.getline(mhs[i].alamat.desa,20);
+        cout << "\tMasukan Kota :";
+        cin.getline(mhs[i].alamat.kota,20);
+        cout << "Masukan Umur :";
+        cin.ignore(1, '\n');
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+
     }
 }
 
